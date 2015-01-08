bb3ee8f931df26156f17e274e23c59fd97dca83e
Ondrej Kolonicny <ondra@ok1cdj.com>
2015-01-08 08:18:22 +0100

    Create init.lua

---
+++ Thermometer-DHT11-Thingspeak/init.lua
@@ -0,0 +1,17 @@
+--init.lua
+print("Setting up WIFI...")
+wifi.setmode(wifi.STATION)
+--modify according your wireless router settings
+wifi.sta.config("SSID","PASSWORD")
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function() 
+if wifi.sta.getip()== nil then 
+print("IP unavaiable, Waiting...") 
+else 
+tmr.stop(1)
+print("Config done, IP is "..wifi.sta.getip())
+dofile("dht11.lua")
+end 
+end)
+
+
