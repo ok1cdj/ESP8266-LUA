9e4e39667e7a1b081a107a80cadb76d5e3149530
Ondrej Kolonicny <ondra@ok1cdj.com>
2015-02-27 13:24:56 +0100

    Update dht11.lua

--- Thermometer-DHT11-Thingspeak/dht11.lua
+++ Thermometer-DHT11-Thingspeak/dht11.lua
@@ -106,7 +106,7 @@ conn=net.createConnection(net.TCP, 0)
 conn:on("receive", function(conn, payload) print(payload) end)
 -- api.thingspeak.com 184.106.153.149
 conn:connect(80,'184.106.153.149') 
-conn:send("GET /update?key=X6GHXZICX1ZMB86C&field1="..Temperature.."."..TemperatureDec.."&field2="..Humidity.."."..HumidityDec.." HTTP/1.1\r\n") 
+conn:send("GET /update?key=XXXXXXXXXX&field1="..Temperature.."."..TemperatureDec.."&field2="..Humidity.."."..HumidityDec.." HTTP/1.1\r\n") 
 conn:send("Host: api.thingspeak.com\r\n") 
 conn:send("Accept: */*\r\n") 
 conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
